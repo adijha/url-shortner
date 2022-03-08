88db4f2b8fd7d88321316f9dc2f057a83e0c5f48
Aditya Jha <mail@adijha.com>
2022-03-09 04:12:42 +0530

    redirect-url-converted-to-gin

--- api/main.go
+++ api/main.go
@@ -3,20 +3,20 @@ package main
 import (
 	"github.com/adijha/url-shortner/routes"
 	"github.com/gin-gonic/gin"
-	"github.com/gofiber/fiber/v2"
 )
 
-func setupRoutes(app *fiber.App) {
+// func setupRoutes(app *fiber.App) {
 
-	app.Static("/", "./public")
-	app.Get("/:url", routes.ResolveURL)
-	app.Post("/api/shorten", routes.ShortenURL)
-}
+// 	app.Static("/", "./public")
+// 	app.Get("/:url", routes.ResolveURL)
+// 	app.Post("/api/shorten", routes.ShortenURL)
+// }
 
 func main() {
 
 	router := gin.Default()
-	router.Static("/", "./public")
+	// router.Static("/", "./public")
+	router.GET("/:url", routes.ResolveURL)
 	// router.GET("/add/:x/:y", add)
 	router.Run(":80")
 
--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -2,29 +2,54 @@ package routes
 
 import (
 	"github.com/adijha/url-shortner/database"
+	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v8"
-	"github.com/gofiber/fiber/v2"
 )
 
-func ResolveURL(c *fiber.Ctx) error {
-	url := c.Params("url")
+func ResolveURL(c *gin.Context) {
+	// get the short URL from the request
+	shortURL := c.Param("url")
 
+	// get the redis client
 	r := database.CreateClient(0)
 	defer r.Close()
 
-	value, err := r.Get(database.Ctx, url).Result()
-
+	// get the URL from the redis database
+	val, err := r.Get(database.Ctx, shortURL).Result()
 	if err == redis.Nil {
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Short URL not found in the database!"})
+		c.JSON(404, gin.H{
+			"error": "URL not found!",
+		})
+		return
 	} else if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Cannot connect to database!"})
+		c.JSON(500, gin.H{
+			"error": "Cannot connect to database!",
+		})
+		return
 	}
 
-	rInr := database.CreateClient(1)
-
-	defer rInr.Close()
-
-	_ = rInr.Incr(database.Ctx, "counter")
-
-	return c.Redirect(value, 301)
+	c.Redirect(301, val)
 }
+
+// func ResolveURL(c *fiber.Ctx) error {
+// 	url := c.Params("url")
+
+// 	r := database.CreateClient(0)
+// 	defer r.Close()
+
+// 	value, err := r.Get(database.Ctx, url).Result()
+
+// 	if err == redis.Nil {
+// 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Short URL not found in the database!"})
+// 	} else if err != nil {
+// 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Cannot connect to database!"})
+// 	}
+
+// 	rInr := database.CreateClient(1)
+
+// 	defer rInr.Close()
+
+// 	_ = rInr.Incr(database.Ctx, "counter")
+
+// 	return c.Redirect(value, 301)
+// }

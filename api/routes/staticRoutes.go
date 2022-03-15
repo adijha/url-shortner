66226b83c9f3505efa1b06573d8319b12cbfaf5e
Aditya Jha <mail@adijha.com>
2022-03-16 02:00:31 +0530

    some-comments-added-for-better-redablity

--- api/routes/staticRoutes.go
+++ api/routes/staticRoutes.go
@@ -10,6 +10,7 @@ func InitializeStaticRoutes(router *gin.Engine) {
 	router.Static("/assets", "./view/assets")
 	router.LoadHTMLGlob("./view/index.html")
 	router.StaticFile("/favicon.ico", "./view/assets/favicon.ico")
+	//show homepage
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})

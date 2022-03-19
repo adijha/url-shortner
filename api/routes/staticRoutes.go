8e4663a7fe02361cb8e1ddae84816549b73b7c90
Aditya Jha <mail@adijha.com>
2022-03-19 21:13:29 +0530

    introducing svelte for frontend

--- api/routes/staticRoutes.go
+++ api/routes/staticRoutes.go
@@ -6,10 +6,20 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// func InitializeStaticRoutes(router *gin.Engine) {
+// 	router.Static("/assets", "./view/assets")
+// 	router.LoadHTMLGlob("./view/index.html")
+// 	router.StaticFile("/favicon.ico", "./view/assets/favicon.ico")
+// 	//show homepage
+// 	router.GET("/", func(c *gin.Context) {
+// 		c.HTML(http.StatusOK, "index.html", nil)
+// 	})
+// }
+
 func InitializeStaticRoutes(router *gin.Engine) {
-	router.Static("/assets", "./view/assets")
-	router.LoadHTMLGlob("./view/index.html")
-	router.StaticFile("/favicon.ico", "./view/assets/favicon.ico")
+	router.Static("/assets", "./view/dist/assets")
+	router.LoadHTMLGlob("./view/dist/index.html")
+	router.StaticFile("/favicon.ico", "./view/dist/favicon.ico")
 	//show homepage
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
